7fc4a86310964ccfd5d050867da403ec3deb654b
astroJeff <andrews@astro.columbia.edu>
2016-08-31 10:40:27 +0300

    likelihood function now runs on a random subset of the data, using a Monte Carlo integration method instead of quad

--- src/power_law_gaussian_no_U.py
+++ src/power_law_gaussian_no_U.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
-from scipy.stats import norm, powerlaw
+from numpy.random import multivariate_normal
+from scipy.stats import norm, powerlaw, truncnorm
 from scipy.integrate import quad
 from scipy.optimize import minimize
 import pickle
@@ -9,51 +10,100 @@ import time
 import corner
 import matplotlib.pyplot as plt
 
-upper = 100.0
+upper = 20.0
 
 
 def ln_posterior(x, data):
 
     sigma, gamma, L = x
 
-    if L<0.0 or L > 6.0:
+    if L<0.0 or L > 0.5:
         return -np.inf
     if sigma<0.0 or sigma>3.0:
         return -np.inf
     if gamma<0.01 or gamma>10.0:
         return -np.inf
 
-    return ln_likelihood(x, data)
+    lp = -np.log(sigma)
+
+    return ln_likelihood(x, data) + lp
+
+
+def ln_posterior_minimize(x, data):
+    N = len(data)
+
+    sigma, gamma, L = x
+
+    if L<0.0 or L > 0.5:
+        return np.inf
+    if sigma<0.0 or sigma>3.0:
+        return np.inf
+    if gamma<0.01 or gamma>10.0:
+        return np.inf
+
+    ll = ln_likelihood(x, data)
+
+    lp = -np.log(sigma)
+
+    return -(ll + lp)
+
+
 
 
 def get_integrand(x, y, gamma, sigma):
     return np.power(x, -(gamma+1.0)) * np.exp(-0.5*((y-x)/sigma)**2)
 
 
-def ln_likelihood(x, data):
+def ln_likelihood(x, data, method='MC'):
+
+    N_samples = 1000
 
     sigma, gamma, L = x
 
-    N = len(data)
+    #N = len(data)
+    N = len(data) / 100
 
     def get_integrand(x, y, gamma, sigma):
         return np.power(x, -(gamma+1.0)) * np.exp(-0.5*((y-x)/sigma)**2)
 
-
-
     integral = np.zeros(N)
-    for i in np.arange(len(data)):
-        args = data[i], gamma, sigma
-        sol = quad(get_integrand, L, upper, args=args)
-        integral[i] = sol[0]
+
+    ran_indices = np.random.randint(len(data), size=N)
+    # for i in np.arange(len(data)):
+
+
+    if method == 'quad':
+        for j in np.arange(N):
+            i = ran_indices[j]
+            args = data[i], gamma, sigma
+            sol = quad(get_integrand, L, upper, args=args)
+            integral[j] = sol[0]
+    elif method == 'MC':
+        ran_x = np.zeros(N)
+        for i in np.arange(N):
+            #a, b = (L - data[ran_indices[i]]) / sigma, (upper - data[ran_indices[i]]) / sigma
+            #ran_x = truncnorm.rvs(a, b, loc=data[ran_indices[i]], scale=sigma, size=N_samples)
+
+            ran_x = norm.rvs(loc=data[ran_indices[i]], scale=sigma, size=N_samples)
+            #ids = np.intersect1d(np.where(ran_x>L)[0], np.where(ran_x<upper)[0])
+            #integral[i] = (1.0/float(N_samples)) * np.sum(np.power(ran_x[ids],-(gamma+1.0)))
+            integral[i] = (1.0/float(N_samples)) * np.sum(np.power(ran_x[ran_x>L],-(gamma+1.0)))
+
+        #ran_x = multivariate_normal(mean=data[ran_indices], cov=sigma*np.identity(N), size=N_samples)
+        #integral = (1.0/float(N_samples)) * np.sum(ran_x[ran_x>L]**(-gamma-1.0), axis=0)
+    else:
+        print "You must enter an appropriate method. Options: 'quad', 'MC'"
+        sys.exit()
+
+
 
     ln_likelihood = - N/2.0 * np.log(2.0*np.pi) \
-                    - N * np.log(sigma) \
                     + N * np.log(gamma) \
                     - N * np.log(np.power(L, -gamma) - np.power(upper, -gamma)) \
                     + np.sum(np.log(integral))
+#                    - N * np.log(sigma) \
 
-    # print sigma, gamma, L, integral
+    #print sigma, gamma, L, np.sum(np.log(integral)), N * np.log(sigma), ln_likelihood
     # return integral
     return ln_likelihood
 
@@ -61,9 +111,9 @@ def ln_likelihood(x, data):
 def run_emcee(data, ndim=3, nwalkers=32, nburn=100, nrun=100):
 
     p0 = np.ones((nwalkers, ndim))
-    p0[:,0] = norm.rvs(loc=2.0, scale=0.1, size=nwalkers) # sigma
-    p0[:,1] = norm.rvs(loc=3.0, scale=0.1, size=nwalkers) # gamma
-    p0[:,2] = norm.rvs(loc=1.0, scale=0.1, size=nwalkers) # L
+    p0[:,0] = norm.rvs(loc=0.1, scale=0.01, size=nwalkers) # sigma
+    p0[:,1] = norm.rvs(loc=4.0, scale=0.1, size=nwalkers) # gamma
+    p0[:,2] = norm.rvs(loc=0.05, scale=0.01, size=nwalkers) # L
 
     # Define sampler
     sampler = emcee.EnsembleSampler(nwalkers, ndim, ln_posterior, args=[data])
@@ -87,8 +137,10 @@ def run_optimize(data, ndim=3):
     def neg_likelihood(x, data):
         return -1.0 * ln_posterior(x, data)
 
+    bounds=((0.001, 1.0), (0.01, 10.0), (0.0001, 1.0))
 
-    res = minimize(neg_likelihood, p0, args=data)
+#    res = minimize(ln_posterior_minimize, p0, args=data, method='L-BFGS-B', bounds=bounds)
+    res = minimize(ln_posterior_minimize, p0, args=data, method='Nelder-Mead', bounds=bounds)
 
     return res
 
@@ -96,16 +148,16 @@ def run_optimize(data, ndim=3):
 
 
 
-# Load data
-if len(sys.argv) != 2:
-    print "Error, must include a filename"
-    sys.exit()
-filename = sys.argv[1]
-filaments = np.load(filename)
-
-
-# Run optimizer
-print run_optimize(filaments)
+# # Load data
+# if len(sys.argv) != 2:
+#     print "Error, must include a filename"
+#     sys.exit()
+# filename = sys.argv[1]
+# filaments = np.load(filename)
+#
+#
+# # Run optimizer
+# print run_optimize(filaments)
 
 
 
